687d87f647efa20988bf2759fb574094c3039b5f
karan chatwani <karan.chatwani@phonepe.com>
2024-09-19 23:38:11 +0530

    fixes

--- JobScheduler/src/Main.java
+++ JobScheduler/src/Main.java
@@ -4,12 +4,9 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         JobScheduleServiceImpl jobScheduleService = JobScheduleServiceImpl.getInstance();
-        Runnable task1 = new Runnable() {
-            @Override
-            public void run() {
+        Runnable task1 = () ->
                 System.out.println("Running task1 at time " + new Date(System.currentTimeMillis()));
-            }
-        };
+
         jobScheduleService.scheduleReccuringJob(task1, 10, 10, TimeUnit.SECONDS);
         System.out.println("Hello world!");
     }
